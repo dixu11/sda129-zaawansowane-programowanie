946ddd41d3afc74e809ffb0c84556f976010c67b
Daniel Szlicht <daniel.szlicht25@gmail.com>
2022-11-27 12:35:32 +0100

    konstruktor toString

--- src/main/java/basics/people/Parrot.java
+++ src/main/java/basics/people/Parrot.java
@@ -5,10 +5,30 @@ import java.util.List;
 
 public class Parrot {
 
-    String name;
-    List<String> knownWords = new ArrayList<>();
-    double chanceToRemember;
-    int maxWordsCount;
+   private String name;
+   private List<String> knownWords;
+   private double chanceToRemember;
+   private int maxWordsCount;
+
+/*    public Parrot(String initialName,double iniChance,int iniMaxWords) {
+        name = initialName;
+        chanceToRemember = iniChance;
+        maxWordsCount = iniMaxWords;
+    }*/
+
+    public Parrot(String name, double chanceToRemember, int maxWordsCount) {
+        this.name = name;
+        this.chanceToRemember = chanceToRemember;
+        this.maxWordsCount = maxWordsCount;
+        knownWords = new ArrayList<>();
+    }
+
+    public Parrot() {
+        name = "Polly";
+        chanceToRemember = 0.5;
+        maxWordsCount = 10;
+        knownWords = new ArrayList<>();
+    }
 
     public void sayHi() {
         System.out.println("Cześć jestem " +name);
@@ -16,5 +36,16 @@ public class Parrot {
         System.out.println("Uczę się ze skutecznością: " + chanceToRemember);
         System.out.println("Mogę znać max: " + maxWordsCount);
     }
+
+    public void learnNewWord(String newWord) {
+        knownWords.add(newWord);
+    }
+
+
+    @Override
+    public String toString() {
+        return "to String zmieniony!";
+    }
+
 
 }
--- src/main/java/basics/people/ParrotDemo.java
+++ src/main/java/basics/people/ParrotDemo.java
@@ -1,5 +1,6 @@
 package basics.people;
 
+import java.awt.*;
 import java.util.Random;
 
 public class ParrotDemo {
@@ -14,16 +15,18 @@ public class ParrotDemo {
         Random random = new Random();
         System.out.println(numbers.toString());
 
+        System.out.println("----------------");
 
-        Parrot parrot = new Parrot();
-        parrot.name = "Gustaw";
+
+        Parrot parrot = new Parrot( "Gustaw", 0.4,3 );
+       /* parrot.name = "Gustaw";
         parrot.chanceToRemember = 0.4;
-        parrot.maxWordsCount = 3;
+        parrot.maxWordsCount = 3;*/
 
-        Parrot parrot2 = new Parrot();
-        parrot2.name = "Bożydar";
+        Parrot parrot2 = new Parrot("Bożydar", 0.8, 4);
+     /*   parrot2.name = "Bożydar";
         parrot2.chanceToRemember = 0.8;
-        parrot2.maxWordsCount = 4;
+        parrot2.maxWordsCount = 4;*/
 
 
         parrot.sayHi();
@@ -36,13 +39,21 @@ public class ParrotDemo {
         System.out.println(parrot2.chanceToRemember);
         System.out.println(parrot2.maxWordsCount);*/
 
-        System.out.println(parrot);
+        System.out.println(parrot.toString());
         System.out.println(parrot2);
 
+        System.out.println("--------");
 
+        parrot.learnNewWord("hello");
+        parrot2.learnNewWord("dajorzeszka");
+        parrot2.learnNewWord("wiecej");
 
+       // System.out.println(parrot.knownWords); //łamie zasade encapsulacji
+       parrot.sayHi();
+       parrot2.sayHi();
 
-
+        Parrot parrot3 = new Parrot();
+        parrot3.sayHi();
 
     }
 
